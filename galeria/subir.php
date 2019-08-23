f1217d9af3f2cfc0419e6dd02dd941321cd7f89c
Carlos Ramos <ramosomontes@gmail.com>
2019-08-23 15:52:38 -0600

    subir

--- galeria/subir.php
+++ galeria/subir.php
@@ -1,3 +1,21 @@
 <?php
+	include 'funciones.php';
+	$conexion = conexion('galerias_practica','root','');
+
+	if(!$conexion){
+		die();
+	}
+
+
+	if($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_FILES)){
+		$scheck = @getimagesize($_FILES['foto']['tmp_name']);
+
+		if($scheck !== false){
+			$carpeta_destino = "fotos/";
+			$archivo_subido = $carpeta_destino . $_FILES['foto']['name'];
+			echo $archivo_subido;
+		}
+	}
+
 	require 'view/subir.view.php';
  ?>
